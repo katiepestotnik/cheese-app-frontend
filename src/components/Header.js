04c73310cef866aa03ea73b346d6e5451939ccf9
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-11-09 20:29:27 -0700

    fix home

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
     return (
         <nav className="nav">
-            <Link to="/cheeses">
+            <Link to="/">
                 <div>CHEESE APP</div>
             </Link>
         </nav>
